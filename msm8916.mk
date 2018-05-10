64079f8511ab539c1d405d420cff4ffd31b26757
Kyle Harrison <khwebmail@gmail.com>
2018-05-10 18:15:16 +0530

    camera: more HAL1 updates  * Update camera flags  * Remove camera hooks from manifests too  * Switch to Snap

--- msm8916.mk
+++ msm8916.mk
@@ -103,12 +103,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl \
     bspcapability \
     camera.msm8916 \
     libshims_camera \
-    Camera2
+    Snap
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
